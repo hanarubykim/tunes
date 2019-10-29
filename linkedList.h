5cd1288e92615c536a53c48aede4cd644e4b68df
Hana <hanarubykim2002@gmail.com>
2019-10-29 10:58:00 -0400

    uwu

--- linkedList.h
+++ linkedList.h
@@ -9,19 +9,22 @@ struct song_node{
 
 
 // insert nodes at the front
-struct node * insert_front(struct song_node *, char artist);
+struct node * insert_front(struct song_node *, char artist[]);
 
 // insert nodes in order (alphabetical by Artist then by Song)
+struct node * alphabetical(struct song_node *, char artist[], char song[]);
 
 // print the entire list
 void print_list(struct song_node *);
 
 // find and return a pointer to a node based on artist and song name
+struct node * findArtist_Song(char artist[], char song[]);
 
 // find and return a pointer to the first song of an artist based on artist name
+struct node * firstSong(char artist[]);
 
 // Return a pointer to random element in the list.
-
+struct node * randomElement(struct song_node *)
 // remove a single specified node from the list
 struct node * remove_node(struct song_node *front, char nameData); //i think we remove by name, not by artist
 
