8de05ca96660db7ad57d7b4f3c8b96846acac59f
Hana <hanarubykim2002@gmail.com>
2019-10-31 11:07:18 -0400

    worked on print list!

--- linkedList.c
+++ linkedList.c
@@ -16,23 +16,35 @@ struct node * insert_front(struct song_node * input, char artist[]){
 }
 
 
-// // insert nodes in order (alphabetical by Artist then by Song)
-// struct node * alphabetical(struct song_node *, char artist[], char song[]);
-//
-// // print the entire list
-// void print_list(struct song_node *);
-//
-// // find and return a pointer to a node based on artist and song name
-// struct node * findArtist_Song(char artist[], char song[]);
-//
-// // find and return a pointer to the first song of an artist based on artist name
-// struct node * firstSong(char artist[]);
-//
-// // Return a pointer to random element in the list.
-// struct node * randomElement(struct song_node *);
-//
-// // remove a single specified node from the list
-// struct node * remove_node(struct song_node *front, char nameData); //i think we remove by name, not by artist
-//
-// // free the entire list
-// struct node * free_list(struct song_node *);
+// insert nodes in order (alphabetical by Artist then by Song)
+struct node * alphabetical(struct song_node *, char artist[], char song[]);
+
+// print the entire list
+void print_list(struct song_node * x){
+  if(x == NULL){
+    printf("Empty.\n");
+  }
+  else{
+    printf("[");
+    while(x->next != NULL){
+      printf("%s by %s |", x->name, x->artist);
+      x = x-> next;
+    }
+    printf("]\n");
+    }
+  }
+
+// find and return a pointer to a node based on artist and song name
+struct node * findArtist_Song(char artist[], char song[]);
+
+// find and return a pointer to the first song of an artist based on artist name
+struct node * firstSong(char artist[]);
+
+// Return a pointer to random element in the list.
+struct node * randomElement(struct song_node *);
+
+// remove a single specified node from the list
+struct node * remove_node(struct song_node *front, char nameData); //i think we remove by name, not by artist
+
+// free the entire list
+struct node * free_list(struct song_node *);
