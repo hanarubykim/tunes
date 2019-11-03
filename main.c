a1e3b2d43744a2edd918e2d2c524d56ab31cf1dc
Christy Guan <cguan00@stuy.edu>
2019-11-03 01:12:24 -0400

    started testing. insert_front doesn't work

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include "library.h"
 
 int main(){
-  srand(time(NULL));
   struct song_node *songs = NULL;
 
   printf("LINKED LIST TESTS\n");
   printf("===============================================\n\n");
 
+  printf("Testing print_list: ");
+  print_list(songs);
+
+  printf("\nTesting insert_front\n");
+
+  printf("Inserting: thunderstruck by ac/dc\n");
+  insert_front(songs, "thunderstruck", "ac/dc");
+
+  printf("Inserting: alive by pearl jam\n");
+  insert_front(songs, "alive", "pearl jam");
+
+  
   printf("Testing print_list: ");
   print_list(songs);
 
