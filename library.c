8d5fda8b1941506bee2db7c567d5f44e2f651762
Hana Kim <hanarubykim2002@gmail.com>
2019-11-03 16:39:46 -0500

    libliblib

--- library.c
+++ library.c
@@ -3,31 +3,56 @@
 #include "linkedList.h"
 
 // Add song nodes.
-void addSong(struct song_node *input[27], char nInput[100], char aInput[100]){}
+void addSong(struct song_node *input[27], char nInput[100], char aInput[100]){
+  int firstLetter = aInput[0] - 'a';
+  input[firstLetter] = insert_order(input[firstLetter], nInput, aInput);
+}
 
 // Search for a song given song and artist name (return a pointer).
-struct song_node * findSong(struct song_node *input[27], char nInput[100], char aInput[100]){}
+struct song_node * findSong(struct song_node *input[27], char nInput[100], char aInput[100]){
+
+
+}
 
 // Search for an artist.
-struct song_node * findArtist(struct song_node *input[27], char aInput[100]){}
+struct song_node * findArtist(struct song_node *input[27], char aInput[100]){
+
+
+}
 
 // Print out all the entries under a certain letter.
-void printForLetter(struct song_node *input[27], char l){}
+void printForLetter(struct song_node *input[27], char l){
+
+
+}
 
 // Print out all the songs of a certain artist
-void printForArtist(struct song_node *input[27], char aInput[100]){}
+void printForArtist(struct song_node *input[27], char aInput[100]){
+
+
+}
+
 
 // Print out the entire library.
-void printLibrary(struct song_node *input[27]){}
+void printLibrary(struct song_node *input[27]){
 
+
+}
 
 // Shuffle - print out a series of randomly chosen songs.
-void shuffle(struct song_node *input[27]){}
+void shuffle(struct song_node *input[27]){
 
+
+}
 
 // Delete a song
-void deleteSong(struct song_node *input[27], char nInput[100], char aInput[100]){}
+void deleteSong(struct song_node *input[27], char nInput[100], char aInput[100]){
 
+
+}
 
 // Clear the library.
-void clearLibrary(struct song_node *input[27]){}
+void clearLibrary(struct song_node *input[27]){
+
+
+}
