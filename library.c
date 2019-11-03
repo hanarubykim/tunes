22415901d3ee37f820d6eb70c7fc87ff8cc82eb8
Hana Kim <hanarubykim2002@gmail.com>
2019-11-03 16:23:19 -0500

    Workin on lib...

--- library.c
+++ library.c
@@ -1,2 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linkedList.h"
+
+// Add song nodes.
+void addSong(struct song_node *input[27], char nInput[100], char aInput[100]){}
+
+// Search for a song given song and artist name (return a pointer).
+struct song_node * findSong(struct song_node *input[27], char nInput[100], char aInput[100]){}
+
+// Search for an artist.
+struct song_node * findArtist(struct song_node *input[27], char aInput[100]){}
+
+// Print out all the entries under a certain letter.
+void printForLetter(struct song_node *input[27], char l){}
+
+// Print out all the songs of a certain artist
+void printForArtist(struct song_node *input[27], char aInput[100]){}
+
+// Print out the entire library.
+void printLibrary(struct song_node *input[27]){}
+
+
+// Shuffle - print out a series of randomly chosen songs.
+void shuffle(struct song_node *input[27]){}
+
+
+// Delete a song
+void deleteSong(struct song_node *input[27], char nInput[100], char aInput[100]){}
+
+
+// Clear the library.
+void clearLibrary(struct song_node *input[27]){}
